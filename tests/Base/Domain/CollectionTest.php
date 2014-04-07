d1a340b058b1cef8bb1a1b4307092f88b90fe50b
ClusmyAssassin <burnfield_r@hotmail.com>
2014-04-07 10:43:04 -0400

    Adding in few new tests to collection tests to make it more robust for the few functions that were added.

--- tests/Base/Domain/CollectionTest.php
+++ tests/Base/Domain/CollectionTest.php
@@ -10,36 +10,68 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
     /** @var Collection */
     private $_collection;
 
+    /** @var stdClass */
+    private $_object;
+
     public function setUp()
     {
-        $this->_collection = new Collection();
+        $this->_object = new stdClass();
+        $this->_object->test = 'test';
+        $this->_collection = new Collection($this->_object);
     }
 
     public function testCreateCollectionOfObjectsWithASingleObject()
     {
-        new Collection(new stdClass());
+        $this->assertEquals($this->_object, $this->_collection[0]);
     }
 
     public function testCreateCollectionOfObjectsFromArray()
     {
-        new Collection(array(new stdClass(), new stdClass(), new stdClass()));
+        $collection = new Collection(array($this->_object, $this->_object, $this->_object));
+        $this->assertEquals(3, count($collection));
     }
 
     public function testAddingNewElementToCollection()
     {
-        $this->_collection[] = new stdClass();
-        $this->_collection->append(new stdClass());
+        $newObj = new stdClass();
+        $newObj->something = 'something';
+        $this->_collection[] = $newObj;
+        $this->assertEquals($newObj, $this->_collection[1]);
     }
 
     public function testSettingAnOffset()
     {
-        $this->_collection->offsetSet(0, new stdClass());
-        $this->_collection[0] = new stdClass();
+        $newObj = new stdClass();
+        $newObj->somethingElse = 'somethingElse';
+        $this->_collection[0] = $newObj;
+        $this->assertEquals($newObj, $this->_collection[0]);
     }
 
     public function testCreateCollectionWithStrings()
     {
         $this->setExpectedException('InvalidArgumentException');
         new Collection('notObject');
+    }
+
+    public function testInCollection()
+    {
+        $this->assertTrue($this->_collection->inCollection($this->_object));
+        $this->assertFalse($this->_collection->inCollection(new stdClass()));
+    }
+
+    public function testUniqueWithEmptyCollection()
+    {
+        $collection = new Collection();
+        $this->assertEquals($collection, $collection->unique());
+    }
+
+    public function testUniqueWithDuplicates()
+    {
+        $newObj = new stdClass();
+        $newObj->val = 'value';
+        $this->_collection[] = $newObj;
+        $this->_collection[] = $newObj;
+        $this->assertEquals(3, count($this->_collection));
+        $this->assertEquals(2, count($this->_collection->unique()));
     }
 }
